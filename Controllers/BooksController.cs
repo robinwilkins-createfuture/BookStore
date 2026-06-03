2373f3ea07ed51129642b13aa3c9cd6ea233b385
Robin Wilkins <robin.wilkins@createfuture.com>
2026-06-03 10:54:56 +0100

    Initial project creation

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+using demo_web_api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demo_web_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    static private List<Book> books = new List<Book>
+    {
+        new() { Id = 1, Title = "The Pragmatic Programmer", Author = "Andrew Hunt", YearPublished = 1999 },
+        new() { Id = 2, Title = "Clean Code", Author = "Robert C. Martin", YearPublished = 2008 },
+        new() { Id = 3, Title = "Domain-Driven Design", Author = "Eric Evans", YearPublished = 2003 },
+        new() { Id = 4, Title = "Lord of the Rings", Author = "J.R.R. Tolkien", YearPublished = 1954 }
+    };
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Book>> GetBooks()
+    {
+        return Ok(books);
+    }
+}
