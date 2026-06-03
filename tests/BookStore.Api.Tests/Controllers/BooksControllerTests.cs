7a97f6e6e13452bafbe82ac8ae38fd3127eccb7e
robinwilkins-createfuture <robin.wilkins@createfuture.com>
2026-06-03 15:54:42 +0100

    Extract a BookDto to decouple API from internal (#2)
    * Extract a BookDto to decouple API from internal
    
    * Ignore Book Id when creating/updating Book

--- tests/BookStore.Api.Tests/Controllers/BooksControllerTests.cs
+++ tests/BookStore.Api.Tests/Controllers/BooksControllerTests.cs
@@ -1,4 +1,5 @@
 using BookStore.Api.Controllers;
+using BookStore.Api.Dtos;
 using BookStore.Api.Models;
 using BookStore.Api.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -47,7 +48,7 @@ public class BooksControllerTests
         var result = _controller.GetBooks();
 
         var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        var books = Assert.IsAssignableFrom<IEnumerable<Book>>(okResult.Value);
+        var books = Assert.IsAssignableFrom<IEnumerable<BookDto>>(okResult.Value);
         Assert.NotEmpty(books);
         Assert.Equal(2, books.Count());
         _bookServiceMock.Verify(service => service.GetBooks(), Times.Once);
@@ -84,7 +85,7 @@ public class BooksControllerTests
         var result = _controller.GetBook(1);
 
         var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        var book = Assert.IsType<Book>(okResult.Value);
+        var book = Assert.IsType<BookDto>(okResult.Value);
         Assert.Equal(1, book.Id);
         Assert.Equal("The Pragmatic Programmer", book.Title);
         Assert.Equal("Andrew Hunt", book.Author);
@@ -103,5 +104,130 @@ public class BooksControllerTests
 
         Assert.IsType<NotFoundResult>(result.Result);
         _bookServiceMock.Verify(service => service.GetBook(999), Times.Once);
+    }
+
+    [Fact]
+    public void CreateBook_WithValidBook_Returns201Created()
+    {
+        var newBookDto = new BookDto
+        {
+            Id = 0, // Id should be ignored when creating a new book
+            Title = "Refactoring",
+            Author = "Martin Fowler",
+            YearPublished = 1999
+        };
+        var createdBook = new Book
+        {
+            Id = 5,
+            Title = newBookDto.Title,
+            Author = newBookDto.Author,
+            YearPublished = newBookDto.YearPublished
+        };
+
+        _bookServiceMock
+            .Setup(service => service.CreateBook(It.IsAny<Book>()))
+            .Returns(createdBook);
+
+        var result = _controller.CreateBook(newBookDto);
+
+        Assert.IsType<CreatedAtActionResult>(result.Result);
+        _bookServiceMock.Verify(
+            service => service.CreateBook(It.Is<Book>(b =>
+                b.Title == newBookDto.Title &&
+                b.Author == newBookDto.Author &&
+                b.YearPublished == newBookDto.YearPublished)),
+            Times.Once);
+    }
+
+    [Fact]
+    public void CreateBook_WithValidBook_ReturnsCreatedBookDto()
+    {
+        var newBookDto = new BookDto
+        {
+            Id = 0, // Id should be ignored when creating a new book
+            Title = "Refactoring",
+            Author = "Martin Fowler",
+            YearPublished = 1999
+        };
+        var createdBook = new Book
+        {
+            Id = 5,
+            Title = newBookDto.Title,
+            Author = newBookDto.Author,
+            YearPublished = newBookDto.YearPublished
+        };
+
+        _bookServiceMock
+            .Setup(service => service.CreateBook(It.IsAny<Book>()))
+            .Returns(createdBook);
+
+        var result = _controller.CreateBook(newBookDto);
+
+        var createdResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+        var returnedBook = Assert.IsType<BookDto>(createdResult.Value);
+        Assert.Equal(5, returnedBook.Id);
+        Assert.Equal("Refactoring", returnedBook.Title);
+        Assert.Equal("Martin Fowler", returnedBook.Author);
+        Assert.Equal(1999, returnedBook.YearPublished);
+        Assert.Equal(nameof(BooksController.GetBook), createdResult.ActionName);
+        Assert.Equal(5, ((BookDto)createdResult.Value!).Id);
+    }
+
+    [Fact]
+    public void CreateBook_WithNullBook_ReturnsBadRequest()
+    {
+        var result = _controller.CreateBook(null!);
+
+        Assert.IsType<BadRequestResult>(result.Result);
+        _bookServiceMock.Verify(service => service.CreateBook(It.IsAny<Book>()), Times.Never);
+    }
+
+    [Fact]
+    public void UpdateBook_WithValidIdAndBook_Returns204NoContent()
+    {
+        var updatedBookDto = new BookDto
+        {
+            Id = 109, // Id should be ignored when updating a book
+            Title = "The Pragmatic Programmer 20th Anniversary Edition",
+            Author = "Andrew Hunt and David Thomas",
+            YearPublished = 2019
+        };
+
+        _bookServiceMock
+            .Setup(service => service.UpdateBook(1, It.IsAny<Book>()))
+            .Returns(true);
+
+        var result = _controller.UpdateBook(1, updatedBookDto);
+
+        Assert.IsType<NoContentResult>(result);
+        _bookServiceMock.Verify(
+            service => service.UpdateBook(1, It.Is<Book>(b =>
+                b.Title == updatedBookDto.Title &&
+                b.Author == updatedBookDto.Author &&
+                b.YearPublished == updatedBookDto.YearPublished)),
+            Times.Once);
+    }
+
+    [Fact]
+    public void UpdateBook_WithInvalidId_Returns404NotFound()
+    {
+        var updatedBookDto = new BookDto
+        {
+            Id = 99, // Id should be ignored when updating a book
+            Title = "Non-existent",
+            Author = "Unknown",
+            YearPublished = 2026
+        };
+
+        _bookServiceMock
+            .Setup(service => service.UpdateBook(999, It.IsAny<Book>()))
+            .Returns(false);
+
+        var result = _controller.UpdateBook(999, updatedBookDto);
+
+        Assert.IsType<NotFoundResult>(result);
+        _bookServiceMock.Verify(
+            service => service.UpdateBook(999, It.IsAny<Book>()),
+            Times.Once);
     }
 }
